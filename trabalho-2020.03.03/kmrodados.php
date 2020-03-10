57b159888af803444f3fca2d3fd483aa105e5b74
VitorMartins28 <vitormartins281999@gmail.com>
2020-03-10 20:04:40 -0300

    correção

--- trabalho-2020.03.03/kmrodados.php
+++ trabalho-2020.03.03/kmrodados.php
@@ -1,9 +1,12 @@
 <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
 <?php
-$distancia = 50;
+$distancia = 0;
 $resultado = $distancia * 2;
 
-
+if($distancia == 0 ){
+   echo "Não existe distância a percorrer";
+   exit;
+}
    echo "A distancia é de".$resultado;
 
 
